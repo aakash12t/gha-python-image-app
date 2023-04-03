46a53fd0e2fbd832be24390a454fb7bee9edd66b
aakash12t <124520536+aakash12t@users.noreply.github.com>
2023-04-03 23:45:45 +0530

    Update app.py
    port changed from 5000 to 5010

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ def calculate_throughput():
 calculate_latency()
 calculate_throughput()
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5010))
     app.run(debug=True,host='0.0.0.0',port=port)
